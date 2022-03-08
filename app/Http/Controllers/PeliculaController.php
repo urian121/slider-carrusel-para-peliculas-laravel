38490d2498fd551de5e7e8d76c6d869139fcfd03
Urian <urian1213viera@gmail.com>
2022-03-08 13:43:55 -0500

    quitando metodos innecesarios

--- app/Http/Controllers/PeliculaController.php
+++ app/Http/Controllers/PeliculaController.php
@@ -7,80 +7,11 @@ use Illuminate\Http\Request;
 
 class PeliculaController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
         $peliculas = Pelicula::all();
         return view('home', compact('peliculas'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Pelicula  $pelicula
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Pelicula $pelicula)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Pelicula  $pelicula
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Pelicula $pelicula)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Pelicula  $pelicula
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Pelicula $pelicula)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Pelicula  $pelicula
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Pelicula $pelicula)
-    {
-        //
-    }
 }
